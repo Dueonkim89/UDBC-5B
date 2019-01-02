8a94690b2ded8b8163a71efece3392c079420305
Dueon Kim <dueonkim89@yahoo.com>
2019-01-02 00:30:15 -0800

    make infura node private

--- truffle.js
+++ truffle.js
@@ -1,8 +1,9 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register');
 
-//import menonmic phrase
+//import menonmic phrase and infura node
 const { menonmic } = require('./menonmic');
+const { infuraNode } = require('./infuraNode');
 
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
@@ -19,7 +20,7 @@ module.exports = {
     },
     rinkeby: {
   		provider: function() {
-  			return new HDWalletProvider(menonmic, 'https://rinkeby.infura.io/v3/a0ab6d59647647dc9fda0824c7c9f107')
+  			return new HDWalletProvider(menonmic, infuraNode)
   		},
   		network_id: '1',
   		gas: 4500000,
