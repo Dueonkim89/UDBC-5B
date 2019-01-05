d4f262b7894b1c00fd8d195b0aec0202f5f48ffc
Dueon Kim <dueonkim89@yahoo.com>
2019-01-05 12:46:58 -0800

    commit hidden files

---
+++ infuraNode.js
@@ -0,0 +1,5 @@
+const infuraNode = 'https://rinkeby.infura.io/v3/a0ab6d59647647dc9fda0824c7c9f107';
+
+module.exports = {
+  infuraNode
+}
---
+++ menonmic.js
@@ -0,0 +1,5 @@
+const menonmic = 'employ unique denial real ticket couch symptom broom fiber want chapter chicken';
+
+module.exports = {
+  menonmic
+}
